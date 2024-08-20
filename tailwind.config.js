c4d433adc504f8a944ff429646fdade8a0266e39
eddieh310 <ejherrer@buffalo.edu>
2024-08-20 13:48:38 -0400

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',     
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        comic: ['"Comic Sans MS', 'Comic Sans', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
